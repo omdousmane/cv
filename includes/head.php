4d99d62ab87db01c13566433c07e1e5da227ea6f
ousmane <omdousmane@gmail.com>
2022-05-18 10:58:27 +0200

    creation de la page about

---
+++ includes/head.php
@@ -0,0 +1,10 @@
+<head>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;700&display=swap" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <script src="https://kit.fontawesome.com/83f4286022.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/style.css">
+  <title>accueil</title>
+</head>
--- watch.bat
+++ watch.bat
@@ -1,2 +1,3 @@
 sass --watch sass/style.scss:css/style.css
 sass --watch sass/cv.scss:css/cv.css
+sass --watch sass/about.scss:css/about.css
